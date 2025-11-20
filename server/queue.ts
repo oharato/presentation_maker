3a94461765e72ef919e016af0c78b0dd127422cf
oharato <oharato@live.jp>
2025-11-21 00:21:27 +0900

    feat: fix redis option

--- server/queue.ts
+++ server/queue.ts
@@ -4,6 +4,8 @@ import Redis from 'ioredis';
 const redisConfig = {
     host: process.env.REDIS_HOST || 'localhost',
     port: parseInt(process.env.REDIS_PORT || '6379', 10),
+    maxRetriesPerRequest: null,
+    enableReadyCheck: false,
 };
 
 // Create Redis clients
